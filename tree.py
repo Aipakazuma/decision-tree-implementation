4aa1665e6c4579ba3d8812efdde20f50db506972
commanderaipa <gundamgundam777@gmail.com>
2018-05-12 14:26:33 +0900

    [modify] 描画はできた...が？なんか変？

--- tree.py
+++ tree.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_iris
 import numpy as np
+from graphviz import Digraph
 
 
 def calc_gini(y):
@@ -32,72 +33,101 @@ class _Node():
         self.threshold = None
 
     def build(self, X, y):
-      n_all_X = X.shape[0]
-      n_X = X.shape[1]
+        self.data_count = X.shape[0]
+        n_X = X.shape[1]
 
-      # 全部同じクラスなら分割する必要がないので処理終了
-      if len(np.unique(y)) == 1:
-          return
+        # 全部同じクラスなら分割する必要がないので処理終了
+        unique, counts = np.unique(y, return_counts=True)
+        if len(unique) == 1:
+            self.label = unique[0]
+            return
 
-      # 分ける前のgini係数
-      gini = calc_gini(y)
-      print('分ける前のgini係数 {}'.format(gini,))
+        # ラベルを多数決で決める
+        self.label = unique[np.argmax(counts)]
 
-      # 一番良いgini係数を補完？していく
-      best_gini_index = 0.0
-      best_x = None
-      best_threshold = None
+        # 分ける前のgini係数
+        gini = calc_gini(y)
+        print('分ける前のgini係数 {}'.format(gini,))
 
-      # 分割候補の計算
-      for n in range(n_X):
-          # 分割候補の作成
-          # 重複削除
-          data_unique = np.unique(x[:, n])
-          # (a ~ y + b ~ z) / 2.0
-          # -> (a + b) / 2.0
-          # -> aとbの中間の値を作成してくれる
-          # -> これを全値でやる
-          points = (data_unique[:-1] + data_unique[1:]) / 2.0
+        # 一番良いgini係数を補完？していく
+        best_gini_index = 0.0
+        best_x = None
+        best_threshold = None
 
-          for threshold in points:
-              # 閾値で2グループに分割
-              y_l = y[X[:, n] < threshold]
-              y_r = y[X[:, n] >= threshold]
+        # 分割候補の計算
+        for n in range(n_X):
+            # 分割候補の作成
+            # 重複削除
+            data_unique = np.unique(x[:, n])
+            # (a ~ y + b ~ z) / 2.0
+            # -> (a + b) / 2.0
+            # -> aとbの中間の値を作成してくれる
+            # -> これを全値でやる
+            points = (data_unique[:-1] + data_unique[1:]) / 2.0
 
-              # 分割後のgini係数を計算
-              gini_l = calc_gini(y_l)
-              gini_r = calc_gini(y_r)
-              p_l = float(y_l.shape[0]) / n_all_X
-              p_r = float(y_r.shape[0]) / n_all_X
-              gini_index = calc_gini_index(gini, gini_l, gini_r, p_l, p_r)
+            for threshold in points:
+                # 閾値で2グループに分割
+                y_l = y[X[:, n] < threshold]
+                y_r = y[X[:, n] >= threshold]
 
-              if gini_index > best_gini_index:
-                  best_gini_index = gini_index
-                  best_x = n
-                  best_threshold = threshold
+                # 分割後のgini係数を計算
+                gini_l = calc_gini(y_l)
+                gini_r = calc_gini(y_r)
+                p_l = float(y_l.shape[0]) / self.data_count
+                p_r = float(y_r.shape[0]) / self.data_count
+                gini_index = calc_gini_index(gini, gini_l, gini_r, p_l, p_r)
 
-      print('best gini {}, best x {}, best threshold {}'.format(best_gini_index,
-                                                                best_x,
-                                                                best_threshold))
+                if gini_index > best_gini_index:
+                    best_gini_index = gini_index
+                    best_x = n
+                    best_threshold = threshold
 
-      self.gini_index = best_gini_index
-      self.x = best_x
-      self.threshold = best_threshold
+        print('best gini {}, best x {}, best threshold {}'.format(best_gini_index,
+                                                                  best_x,
+                                                                  best_threshold))
 
-      # 左側の分割
-      conditions_l = X[:, self.x] < self.threshold
-      x_l = X[conditions_l]
-      y_l = y[conditions_l]
-      self.left = _Node()
-      self.left.build(x_l, y_l)
+        # 不純度が減らなければ終了
+        if best_gini_index == 0.0:
+            return
 
-      # 右側の分割
-      conditions_r = X[:, self.x] >= self.threshold
-      x_r = X[conditions_r]
-      y_r = y[conditions_r]
-      self.right = _Node()
-      self.right.build(x_l, y_l)
+        self.gini_index = best_gini_index
+        self.x = best_x
+        self.threshold = best_threshold
 
+        # 左側の分割
+        conditions_l = X[:, self.x] < self.threshold
+        x_l = X[conditions_l]
+        y_l = y[conditions_l]
+        self.left = _Node()
+        self.left.build(x_l, y_l)
+
+        # 右側の分割
+        conditions_r = X[:, self.x] >= self.threshold
+        x_r = X[conditions_r]
+        y_r = y[conditions_r]
+        self.right = _Node()
+        self.right.build(x_r, y_r)
+
+    def get_tree_info(self):
+        _name = 'name_{}'.format(np.random.uniform())
+        if self.x is None:
+            # 葉の場合
+            return {
+                'label': self.label,
+                'data_count': self.data_count,
+                'name': _name
+            }
+
+        # 節の場合
+        left = self.left.get_tree_info()
+        right = self.right.get_tree_info()
+        return {
+            'left': left,
+            'right': right,
+            'data_count': self.data_count,
+            'name': _name
+        }
+    
 
 if __name__ == '__main__':
     # test
@@ -110,3 +140,26 @@ if __name__ == '__main__':
     root = _Node()
     root.build(X, y)
 
+    # formatはpngを指定(他にはPDF, PNG, SVGなどが指定可)
+    g = Digraph(format='png')
+    g.attr('node', shape='circle')
+    tree_dict = root.get_tree_info()
+    import pprint
+    pprint.pprint(tree_dict)
+
+    def make_graph(d, g):
+        if 'label' in d:
+            # 葉
+            g.node(d['name'])
+        else:
+            # 節
+            g.edge(d['name'], d['left']['name'], label=str(d['left']['data_count']))
+            g.edge(d['name'], d['right']['name'], label=str(d['right']['data_count']))
+
+            make_graph(d['left'],  g)
+            make_graph(d['right'], g)
+
+    _name = 'root'
+    g.node(_name, color='pink')
+    make_graph(tree_dict, g)
+    g.render()
